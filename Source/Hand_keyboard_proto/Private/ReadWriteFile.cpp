174f91eb20bb937b1613591222a126c4b3f55462
YichengZhang <1343491089@qq.com>
2022-12-03 02:40:11 -0500

    Finish file read

--- Source/Hand_keyboard_proto/Private/ReadWriteFile.cpp
+++ Source/Hand_keyboard_proto/Private/ReadWriteFile.cpp
@@ -25,3 +25,56 @@ bool UReadWriteFile::SaveArrayText(FString SaveDirectory, FString FileName, TArr
 
 	return FFileHelper::SaveStringToFile(FinalString, *SaveDirectory);
 }
+
+bool UReadWriteFile::ReadDataRecordFile(FString SaveDirectory, FString FileName, TArray<FDataRecordUnitCpp>& PastDataList)
+{
+	// Set file path
+	SaveDirectory += "\\";
+	SaveDirectory += FileName;
+
+	if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*SaveDirectory)) {
+		return false;
+	}
+
+	// Read every line into Texts
+	TArray<FString> Texts;
+	FFileHelper::LoadFileToStringArray(Texts, *SaveDirectory);
+	
+	// Split each line, and store them
+	PastDataList.Add(FDataRecordUnitCpp()); // The first one
+	for (int i = 1; i < Texts.Num(); i++) {
+		TArray<FString> Words;
+		TArray<FString> WordsPre;
+		Texts[i].ParseIntoArray(Words, TEXT(","), false);
+		if (i == 0) {
+			PastDataList.Add(FDataRecordUnitCpp());
+		}
+		else {
+			Texts[i-1].ParseIntoArray(WordsPre, TEXT(","), false);
+			if (Words[0] != WordsPre[0]) {
+				// Means this is a new letter
+				PastDataList.Add(FDataRecordUnitCpp());
+			}
+		}
+		PastDataList[PastDataList.Num() - 1].Letter = Words[0];
+		PastDataList[PastDataList.Num() - 1].LeftLocationList.Add(FVector(FCString::Atof(*Words[1]), FCString::Atof(*Words[2]), FCString::Atof(*Words[3])));
+		PastDataList[PastDataList.Num() - 1].RightLocationList.Add(FVector(FCString::Atof(*Words[4]), FCString::Atof(*Words[5]), FCString::Atof(*Words[6])));
+		PastDataList[PastDataList.Num() - 1].LeftDistance = FCString::Atof(*Words[7]);
+		PastDataList[PastDataList.Num() - 1].RightDistance = FCString::Atof(*Words[8]);
+		PastDataList[PastDataList.Num() - 1].TimeList.Add(FCString::Atof(*Words[9]));
+		PastDataList[PastDataList.Num() - 1].PreviousLetter = Words[10];
+	}
+
+	return true;
+}
+
+bool UReadWriteFile::ReadAllFileNames(FString SaveDirectory, TArray<FString>& AllFileNames)
+{
+	// Set file path
+	SaveDirectory += "\\myfilenames.csv";
+
+	if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*SaveDirectory)) {
+		return false;
+	}
+	return FFileHelper::LoadFileToStringArray(AllFileNames, *SaveDirectory);
+}
--- Source/Hand_keyboard_proto/Public/ReadWriteFile.h
+++ Source/Hand_keyboard_proto/Public/ReadWriteFile.h
@@ -9,12 +9,56 @@
 /**
  * 
  */
+USTRUCT(BlueprintType)
+struct HAND_KEYBOARD_PROTO_API FDataRecordUnitCpp
+{
+	GENERATED_BODY()
+		
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Letter;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FVector> LeftLocationList;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FVector> RightLocationList;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float LeftDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float RightDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<float> TimeList;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString PreviousLetter;
+
+	FDataRecordUnitCpp()
+	{
+		Letter = FString(TEXT(""));
+		LeftDistance = 0.0;
+		RightDistance = 0.0;
+		PreviousLetter = FString(TEXT(""));
+	}
+};
+
+
 UCLASS()
 class HAND_KEYBOARD_PROTO_API UReadWriteFile : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
 public:
-		UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "Save"))
-			static bool SaveArrayText(FString SaveDirectory, FString FileName, TArray<FString> SaveText, bool AllowOverWriting);
+	UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "Save"))
+		static bool SaveArrayText(FString SaveDirectory, FString FileName, TArray<FString> SaveText, bool AllowOverWriting);
+
+	UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "Read"))
+		static bool ReadDataRecordFile(FString SaveDirectory, FString FileName, TArray<FDataRecordUnitCpp>& PastDataList);
+
+	UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "FileName"))
+		static bool ReadAllFileNames(FString SaveDirectory, TArray<FString>& AllFileNames);
 };
+
+
